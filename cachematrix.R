354ddbe89df9c5ab1c10b3fbeb275c462b4fff67
hender1288 <36714902+hender1288@users.noreply.github.com>
2018-03-12 21:44:46 +0100

    Add files via upload
    Final code file corresponding to ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R aims to reuse the data store in cache in case that the 
+## function is executed with the same arguments as the previous execution
+## in order to save time and resources computing the matrix inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix receive an inversable matrix as an argument
+## and returns a list of 4 elements which are:
+## [1] a function that sets the matrix to be inversed -->set
+## [2] a function to get the matrix to be inversed --> get
+## [3] a function for compute the inverse matrix -->setinv
+## [4] a function for obtain the inverse matrix -->getinv
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(mtrx = matrix()) {
+  
+  inv <- NULL
+  set <- function(y) {
+    mtrx <<- y
+    inv <<- NULL
+  }
+  get <- function() mtrx
+  setinv <- function(inv) inv <<- solve(mtrx)
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv= setinv,
+       getinv = getinv)
+  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cachesolve will calculate the inverse recieving a list produced by makeCacheMatrix
+cacheSolve  <- function(chmtrx, ...) {
+  inv <- chmtrx$getinv()
+  if(!is.null(inv)) {
+    print("getting data fro cache")
+    return(inv)
+  }
+  data <- chmtrx$get()
+  inv <- solve(data, ...)
+  chmtrx$setinv(inv)
+  inv
 }
